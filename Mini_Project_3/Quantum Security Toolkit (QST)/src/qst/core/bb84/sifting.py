ff8292a2fd21e52ad2b6e0c807b591c8f1327abc
Shlok Thorat <shlok.thorat.cyb@ghrcem.raisoni.net>
2026-09-10 23:10:19 +0530

    refactor(bb84): harden and optimize sifting module

--- Mini_Project_3/Quantum Security Toolkit (QST)/src/qst/core/bb84/sifting.py
+++ Mini_Project_3/Quantum Security Toolkit (QST)/src/qst/core/bb84/sifting.py
@@ -7,9 +7,9 @@ References:
 
 from typing import Sequence
 
+from qst.core.bb84.validators import validate_bb84_bits
 from qst.exceptions.validation import ValidationError
 from qst.models.results import ReconciliationResult, SiftedKeyResult
-from qst.core.bb84.validators import validate_bb84_bits
 
 
 class KeySiftingService:
