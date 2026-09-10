eb42deb03f8d7bc8382016c4dd23f24e7a03e53a
Shlok Thorat <shlok.thorat.cyb@ghrcem.raisoni.net>
2026-09-10 23:10:24 +0530

    refactor(exceptions): standardize QST error hierarchy and codes

--- Mini_Project_3/Quantum Security Toolkit (QST)/src/qst/exceptions/__init__.py
+++ Mini_Project_3/Quantum Security Toolkit (QST)/src/qst/exceptions/__init__.py
@@ -4,13 +4,13 @@ References:
     Docs/10_API_SPECIFICATION.md §6
 """
 
+from qst.correction.exceptions import CorrectionError
 from qst.exceptions.base import QSTError
 from qst.exceptions.configuration import ConfigurationError
 from qst.exceptions.export import ExportError
 from qst.exceptions.simulation import SimulationError
 from qst.exceptions.validation import ValidationError
 from qst.exceptions.visualization import VisualizationError
-from qst.correction.exceptions import CorrectionError
 from qst.privacy.exceptions import PrivacyAmplificationError
 from qst.secret.exceptions import SecretKeyError
 
